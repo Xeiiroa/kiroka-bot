b9aa3c21dd951ed80705ef9195912e1bc3c0b449
Sintiria <Jaydentomlinson4@gmail.com>
2023-08-08 15:51:12 -0400

    Update Lol.py

--- cogs/Lol.py
+++ cogs/Lol.py
@@ -13,13 +13,23 @@ class Lol(commands.Cog):
         
     @commands.command
     async def leagueprofile(self, ctx, summonername: str, region: str):
-        
         regions = ["BR1","EUN1","EUW1","JP1","KR","LA1","LA2","NA1","OC1","PH2","RU","SG2","TH2","TR1","TW2","VN2"]
-        response = requests.get(f"https://{region.lower()}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonername.replace(' ','%20')}?api_key={RIOT_KEY}")
-        player_info= response.json()
+        
+        # getting data for summoner name, profile icon id, level, and account idea for further inspection
+        summonerinfolink = requests.get(f"https://{region.lower()}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonername.replace(' ','%20')}?api_key={RIOT_KEY}")
+        player_info= summonerinfolink.json()
         player_pfp = player_info["profileIconId"]
         player_level = player_info["summonerLevel"]
         summoneriD = player_info["id"]
+        
+        #getting data for user rank
+        summonerrankinfo =
+        
+        
+        
+        
+        
+        
         
         mastery = requests.get(f"https://{region.lower()}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{summoneriD}")
         player_mastery = mastery.json()
@@ -32,6 +42,9 @@ class Lol(commands.Cog):
             elif i == "championPoints":
                 champdict["masterypoints"].extend(player_mastery["championPoints"])
             #create a counter that only allows the loop to go 3 times
+            
+            
+        
                 
                 
             
@@ -43,9 +56,8 @@ class Lol(commands.Cog):
         
         
         
-        
+        #the embed message for the player stats and all its parameters
         embed_message = discord.Embed(title=f"Profile for {summonername}")
-        #summoner pfp
         embed_message.set_thumbnail(url=f"http://ddragon.leagueoflegends.com/cdn/13.15.1/img/profileicon/{player_pfp}.png")
         embed_message.add_field(name="Summoner level" , value=player_level, inline= False)
         embed_message.add_field(name="Ranked Solo/Duo", value=..., inline=...)
@@ -61,6 +73,7 @@ class Lol(commands.Cog):
     @leagueprofile.error
     async def leagueprofile_error(self,ctx):
         ...
+        
     
     
     @commands.command
