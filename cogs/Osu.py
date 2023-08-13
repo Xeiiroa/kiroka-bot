104a040b9827c8f9260a69173f5ec800b6731d9b
Jay <Jaydentomlinson4@gmail.com>
2023-08-13 12:44:59 -0700

    creating osu file

--- cogs/Osu.py
+++ cogs/Osu.py
@@ -4,14 +4,21 @@ from tokens import *
 import requests
 import json
 
+from ossapi import Ossapi
+
 class Osu(commands.Cog):
     def __init__(self, client):
         self.client = client
+        api = Ossapi(OSU_CLIENT_ID, OSU_SECRET)
+        
      
     #Ready event    
     @commands.Cog.listener()
     async def on_ready(self):
         print("Osu is ready.")
+        
+        
+    api = Ossapi(client_id, client_secret)
         
         
         
