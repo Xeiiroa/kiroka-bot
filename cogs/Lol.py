d1a24821c424764ef9daa0c936f24ba58175f7f9
Sintiria <Jaydentomlinson4@gmail.com>
2023-08-19 01:05:17 -0400

    lol stats error check

--- cogs/Lol.py
+++ cogs/Lol.py
@@ -14,7 +14,8 @@ class Lol(commands.Cog):
     async def lolstats(self, ctx, summonername:str, region = "na1"):
         regions = ["br1","eun1","euw1","jp1","kr","la1","la2","na1","oc1","ph2","ru","sg2","th2","tr1","tw2","vn2"]
         if region.lower() not in regions:
-            raise commands.BadArgument("region not found")
+            await ctx.send("non-valid region provided" + "\n" + "list of valid regions:" + "\n" +
+                           "br1, eun1, euw1, jp1, kr, la1, la2, na1, oc1, ph2, ru, sg2, th2, tr1, tw2, vn2")
         
         summonerid = self.get_summonerid(summonername, region)
         if summonerid == None:
