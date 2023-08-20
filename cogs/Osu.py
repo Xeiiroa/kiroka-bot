c2fa71c7bdf276af4463764e7dc58f959655b531
Sintiria <Jaydentomlinson4@gmail.com>
2023-08-20 17:28:20 -0400

    Polished osustats command for testing

--- cogs/Osu.py
+++ cogs/Osu.py
@@ -3,41 +3,52 @@ from discord.ext import commands
 from tokens import *
 import requests
 import json
+import datetime
 
 from ossapi import Ossapi
 
 class Osu(commands.Cog):
     def __init__(self, client):
         self.client = client
-        api = Ossapi(OSU_CLIENT_ID, OSU_SECRET)
-        api_key=OSU_KEY
         
-     
+        
     #Ready event    
     @commands.Cog.listener()
     async def on_ready(self):
         print("Osu is ready.")
         
-        
+    #gets users osu stats    
     @commands.command()
     async def osustats(self, ctx, playername):
-        profilelink = (f"https://osu.ppy.sh/api/get_user")
-        params = {
-            "k": OSU_KEY,
-            "u": playername
-        }
-    
-    
-        response = requests.get(profilelink, params=params)
-        
-        if response.status_code == 200:
-            player_data = response.json()[0]
-            #theres more links in this but i have to print the actual json file which i need my pc for
-            print("Player ID:", player_data["user_id"])
-            print("Player Username:", player_data["username"])
-            print("Player Country:", player_data["country"])
-        
-    
+        profile = self.get_profile(playername)
+        if profile == None:
+            await ctx.send(f"player, {playername} not found")
+        else:   
+            player_url = f"https://osu.ppy.sh/users/{profile['userid']}"
+            avatar_url=f"https://a.ppy.sh/{profile['userid']}"
+
+
+            embed = discord.Embed(
+                title=f"{playername}'s stats",
+                color=discord.Color.blue(),
+                url=player_url
+            )
+            
+            
+            embed.set_thumbnail(avatar_url)
+            
+            
+            embed.add_field(name="Rank", value=f"{profile['rank']}  {profile['pp']}pp")
+            embed.add_field(name="Level", value=profile['level'])
+            embed.add_field(name="Accuracy", value=profile['accuracy'])
+            embed.add_field(name="Score", value=f"Ranked Score: {profile['rscore']}\nTotal Score: {profile['tscore']}")
+            embed.add_field(name="Playtime", value=f"{profile['playtime']} ({profile['playcount']}Plays)")
+
+
+            await ctx.send(embed=embed)
+   
+   
+   
     @commands.command()
     async def recentplay(self, ctx, playername):
         key = OSU_KEY
@@ -104,27 +115,36 @@ class Osu(commands.Cog):
         response = requests.get(url, params=params)
         data=response.json()
         
-        #TODO append these things to the dict and return them
-        """
-        #userid, 
-        # playcount, 
-        # rankedscore, 
-        # totalscore, 
-        # country, 
-        # globalrank, 
-        # countryrank, 
-        # level(remove all decimal points)
-        # pp(remove all decimal points)
-        # accuracy(remove all decimal points besides the last 2)
-        # playtime(default is in seconds so ill have to use datetime to change it to days hours min)
-        """
+        if data.status_code == 404:
+            return None
+        else:
+            #TODO append these things to the dict and return them
+            stats['userid'] = data[0]['user_id']
+            stats['playcount'] = data[0]['playcount']
+            stats['rscore'] = data[0]['ranked_score']
+            stats['tscore'] = data[0]['total_score']
+            stats['country'] = data[0]['country']
+            stats['rank'] = f"{data[0]['pp_rank']}({data[0]['country']} Rank:{data[0]['pp_country_rank']})"
+            stats['level'] = ("%d" % float(data[0]["level"]))
+            stats['pp'] = ("%d" % float(data[0]["pp_raw"]))
+            stats['accuracy'] = f"{float(data[0]['accuracy']): .2f}"
+            
+            playtime_seconds = int(data[0]['total_seconds_played'])
+            days, remainder = divmod(playtime_seconds, 86400)
+            hours, remainder = divmod(remainder, 3600)
+            minutes, _ = divmod(remainder, 60)
+    
+            stats['Playtime'] = f"{days}D {hours}H {minutes}M"
+            
+            return stats
         
+    
         #*remember that the userid is what leads to the playerlink so you can hyperlink it to the embed link later on
-        stats += ...
+        
         
     #get the count for ranks acheived (100 s ranks, 400 a ranks, etc)
     def get_ranks(self, playername):
-        stats={}
+        ranks={}
         url = "https://osu.ppy.sh/api/get_user"
         params = {
             "k": OSU_KEY,
@@ -133,15 +153,14 @@ class Osu(commands.Cog):
         response = requests.get(url, params=params)
         data=response.json()
         
-        #TODO append these things to the dict and return them
-        """
-        #silver ss ranks
-        #ss ranks
-        #silver s ranks
-        #s ranks
-        #a ranks
-        """
+        ranks['silv_ss'] = data[0]['count_rank_ssh']
+        ranks['silv_s'] = data[0]['count_rank_sh']
+        ranks['ss']  = data[0]['count_rank_ss']
+        ranks['s']  = data[0]['count_rank_s']
+        ranks['a']  = data[0]['count_rank_a']
         
+        return ranks    
+       
         
         
         
